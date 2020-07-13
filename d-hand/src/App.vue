6bb233f81e2aefbf730e7526f15449dcbdf48791
ya95123 <ya95123@gmail.com>
2020-07-13 15:19:10 +0800

    document：initail

---
+++ d-hand/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app id="app">
+    123
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+
+  })
+}
+</script>
---
+++ d-hand/src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      title: '猜拳｜決定之手'
+    }
+  }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+// to 即將訪問的頁面
+// from 是來源頁面
+// next 是採取的導向動作
+router.afterEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
+export default router
