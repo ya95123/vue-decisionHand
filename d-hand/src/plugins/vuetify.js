69b8818c6f01636bac65dd59b7632ecb35518038
ya95123 <ya95123@gmail.com>
2020-07-14 11:46:42 +0800

    plugins：setting color

--- d-hand/src/plugins/vuetify.js
+++ d-hand/src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        // 新增顏色
+        dhyellow: '#F9e54E',
+        dhorange: '#F8981D',
+        dhred: '#E12E4B',
+        dhblue: '#5BBDC8'
+      }
+    }
+  }
 })
