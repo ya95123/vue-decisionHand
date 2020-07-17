6109504d0fbf3742136184050f1538dc95ddfac4
ya95123 <ya95123@gmail.com>
2020-07-17 16:37:38 +0800

    bottle：test rotate

--- d-hand/src/views/Bottle.vue
+++ d-hand/src/views/Bottle.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="bottle" class="d-flex justify-center align-center">
     <div class="game d-flex justify-center align-center">
+      <!-- {{data.deg}} -->
       <!-- bottle -->
-      <img src="../assets/wine.svg" alt="酒瓶">
+      <img id="wine" @click="rotate" src="../assets/wine.svg" alt="酒瓶">
     </div>
   </div>
 </template>
 
 <script>
-
+const rand = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min)
+}
+// const wineRotate = rand(3240, 3600)
 export default {
   name: 'Bottle',
   data: () => ({
+    deg: 0
+  }),
+  methods: {
+    rotate () {
+      this.style.transform = `rotate(${rand(3240, 3600)}deg)`
 
-  })
+      // document.getElementById('wine').style.transform = `rotate(${rand(3240, 3600)}deg)`
+      // document.getElementById('wine').style.transform = 'rotate(this.$data.deg)'
+      console.log(rand(0, 360))
+    }
+  }
 }
 </script>
