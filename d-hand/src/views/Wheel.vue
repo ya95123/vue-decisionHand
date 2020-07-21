c2f494245c4e4408c0f70ee672f7df552aac638b
ya95123 <ya95123@gmail.com>
2020-07-21 17:32:35 +0800

    css：test wheel text

--- d-hand/src/views/Wheel.vue
+++ d-hand/src/views/Wheel.vue
@@ -3,7 +3,7 @@
     <div class="game d-flex justify-center align-center">
       <v-icon id="wheelSet" class="gameSet">mdi-cog-outline</v-icon>
       <!-- 大圓 -->
-      <div id="turnTable">
+      <div class="turnTable" style="z-index:0;">
         <!-- 內容 -->
         <!-- <div
           class="part d-flex justify-center align-center"
@@ -12,15 +12,18 @@
         >
           {{item}}
         </div> -->
-        <!-- 右半圓 -->
-        <!-- <div class="halfRound rightRound"></div> -->
-        <!-- 左半圓 -->
+
         <div class="test1"></div>
         <div class="test2"></div>
-        <div class="test3"></div>
+        <!-- <div class="test3 fxcenter">3</div> -->
+        <!-- 左半圓框：給最後一個值放的 -->
         <div class="halfRound leftRound">
-          <div class="test4"></div>
+          <div class="test3"></div>
         </div>
+        <!-- 文字區 -->
+        <div class="wheelText1">1111111</div>
+        <div class="wheelText2">2222222</div>
+        <div class="wheelText3">3333333</div>
       </div>
     </div>
   </div>
