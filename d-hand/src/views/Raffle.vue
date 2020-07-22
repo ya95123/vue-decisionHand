ea260dd287f5b9a4323d185bb5a437ded2fab331
ya95123 <ya95123@gmail.com>
2020-07-22 11:26:29 +0800

    raffle：todo

--- d-hand/src/views/Raffle.vue
+++ d-hand/src/views/Raffle.vue
@@ -6,10 +6,11 @@
         <v-icon class="gameSet mt-2">mdi-history</v-icon>
       </div>
     <!-- 箱子 class shake-constant shake-hard -->
+    <!-- TODO 抽出來時，做有手遮90%效果，製造刺激感(有時間就做) -->
     <div id="box" class="d-flex justify-center">
-      <!-- TODO 紙條 -->
-      <div class="paper"></div>
-      <div class="paper" style="left:10%;"></div>
+      <!-- TODO 紙條：開始後做翻面效果，好後做 shake + 紙條飄移 + (紙箱變色) -->
+      <div class="paper">...</div>
+      <div class="paper" style="left:30%;">321</div>
     </div>
 
     </div>
