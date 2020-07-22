b6d353275f8b98d308b9786e9e83fa4e6fac4332
ya95123 <ya95123@gmail.com>
2020-07-22 11:11:25 +0800

    raffle：box + paper

--- d-hand/src/views/Raffle.vue
+++ d-hand/src/views/Raffle.vue
@@ -5,7 +5,13 @@
         <v-icon class="gameSet">mdi-cog-outline</v-icon>
         <v-icon class="gameSet mt-2">mdi-history</v-icon>
       </div>
-    raffle
+    <!-- 箱子 class shake-constant shake-hard -->
+    <div id="box" class="d-flex justify-center">
+      <!-- TODO 紙條 -->
+      <div class="paper"></div>
+      <div class="paper" style="left:10%;"></div>
+    </div>
+
     </div>
   </div>
 </template>
