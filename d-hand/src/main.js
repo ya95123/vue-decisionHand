1695fa8871d090b9508b7c66cf47cece596ffdf3
ya95123 <ya95123@gmail.com>
2020-07-23 14:06:06 +0800

    scroll bar：vuescroll.js

--- d-hand/src/main.js
+++ d-hand/src/main.js
@@ -5,6 +5,37 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import '../dist/style.min.css'
+// scroll bar
+import vuescroll from 'vuescroll'
+
+Vue.use(vuescroll, {
+  // 在這裡設置全局默認配置
+  ops: {
+    scrollPanel: {
+      initialScrollY: false,
+      initialScrollX: false,
+      scrollingX: false,
+      scrollingY: true,
+      speed: 300,
+      easing: undefined,
+      verticalNativeBarPos: 'right',
+      maxHeight: 500,
+      maxWidth: 400
+    },
+    bar: {
+      showDelay: 500,
+      onlyShowBarOnScroll: true,
+      keepShow: false,
+      background: '#c1c1c1',
+      opacity: 1,
+      hoverStyle: false,
+      specifyBorderRadius: false,
+      minSize: false,
+      size: '6px',
+      disable: false
+    }
+  }
+})
 
 Vue.config.productionTip = false
 
