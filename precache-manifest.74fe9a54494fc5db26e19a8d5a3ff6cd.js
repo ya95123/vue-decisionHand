3384161bdb780e7e00d120e516de51a6abfb161f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-04 04:09:35 +0000

    Deploy ya95123/vue-decisionHand to github.com/ya95123/vue-decisionHand.git:gh-pages

---
+++ precache-manifest.74fe9a54494fc5db26e19a8d5a3ff6cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41915ca3092f5581fff",
+    "url": "/css/app.492d0295.css"
+  },
+  {
+    "revision": "3709b43f3534267c3c01",
+    "url": "/css/chunk-vendors.441fb6cf.css"
+  },
+  {
+    "revision": "429b1e5d5b0ac6c221f92b5b997ebf85",
+    "url": "/img/wine.429b1e5d.svg"
+  },
+  {
+    "revision": "cc749ba346d3375eff765126fc259b0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c41915ca3092f5581fff",
+    "url": "/js/app.65a9a6d9.js"
+  },
+  {
+    "revision": "c0a0068e62d8372949ce",
+    "url": "/js/bottle.c50d2a98.js"
+  },
+  {
+    "revision": "3709b43f3534267c3c01",
+    "url": "/js/chunk-vendors.af0ad950.js"
+  },
+  {
+    "revision": "35b804cfa47646ff2739",
+    "url": "/js/group.5b8550aa.js"
+  },
+  {
+    "revision": "2eba215e88dc6811e6e2",
+    "url": "/js/raffle.69fe3c44.js"
+  },
+  {
+    "revision": "24274ece11541776f5b0",
+    "url": "/js/wheel.1594f0b6.js"
+  },
+  {
+    "revision": "86a2ba03ea3f300fb5390207065072d0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
