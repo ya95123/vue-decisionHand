1a0d7c787c5bf50410117cef9700059a45d20229
ya95123 <ya95123@gmail.com>
2020-08-04 14:45:17 +0800

    backend：renew files

--- db.js
+++ db.js
@@ -10,47 +10,35 @@ mongoose.connect(process.env.DBURL)
 mongoose.plugin(beautifyUnique)
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    minlength: [4, '暱稱最少 4 個字'],
+    maxlength: [20, '暱稱最多 20 個字'],
+    unique: '暱稱已被使用',
+    required: [true, '請輸入暱稱']
+  },
   account: {
     type: String,
-    minlength: [4, '帳號必須 4 個字以上'],
-    maxlength: [20, '帳號必須 20 個字以下'],
-    unique: '帳號已使用',
+    minlength: [4, '帳號最少 4 個字'],
+    maxlength: [20, '帳號最多 20 個字'],
+    unique: '帳號已被註冊',
     required: [true, '請輸入帳號']
   },
   password: {
     type: String,
+    minlength: [4, '帳號最少 4 個字'],
     required: [true, '請輸入密碼']
   }
 }, {
   versionKey: false
 })
 
-const fileSchema = new Schema({
-  user: {
-    type: String,
-    required: [true, '請輸入使用者名稱']
-  },
-  description: {
-    type: String,
-    maxlength: [200, '文字必須為200字以下']
-  },
-  name: {
-    type: String,
-    required: [true, '沒有檔案名稱']
-  }
-}, {
-  versionKey: false
-})
-
 // process.env.COLLECTION_USER =>'users'
-// process.env.COLLECTION_FILE =>'files'
 // 連到 env 檔
 const users = mongoose.model(process.env.COLLECTION_USER, userSchema)
-const files = mongoose.model(process.env.COLLECTION_FILE, fileSchema)
 const connection = mongoose.connection
 
 export default {
   users,
-  files,
   connection
 }
