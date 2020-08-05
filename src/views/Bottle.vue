113436c03c1ea3b87d95e7af76d83aabd987a7fa
ya95123 <ya95123@gmail.com>
2020-08-06 00:37:50 +0800

    bottle：tip

--- src/views/Bottle.vue
+++ src/views/Bottle.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="bottle" class="d-flex justify-center align-center">
     <div class="game d-flex justify-center align-center">
-      <!-- {{data.deg}} -->
+      <!-- tips -->
+      <span v-if="!start" id="tip">點擊酒瓶旋轉</span>
       <!-- bottle -->
       <img id="wine" @click="rotate" src="../assets/wine.svg" alt="酒瓶">
     </div>
@@ -15,12 +16,17 @@ const rand = (min, max) => {
 export default {
   name: 'Bottle',
   data: () => ({
+    start: false,
     // 3240 9圈
     deg: 3240,
+    // 第 n 圈
     n: 1
   }),
   methods: {
     rotate () {
+      // 提示文字
+      this.$data.start = true
+      // 轉瓶子
       let turn = this.$data.deg * this.$data.n + rand(0, 360)
       document.getElementById('wine').style.transform = `rotate(${turn}deg)`
       console.log(turn)
